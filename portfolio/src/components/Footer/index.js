a7c9cf822b10fc62839303c713786599349017e8
Kieran Hannagan <kieranhannagan@Kierans-MacBook-Pro.local>
2022-03-11 17:39:48 -0600

    built overall footer structure

---
+++ portfolio/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Footer() {
+
+    return (
+        <footer className="footer navbar-fixed-bottom">
+            <div>
+            <p>Footer</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
