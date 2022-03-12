2fa1d2bd3db57183f8f31e09163d8157e775e86f
Kieran Hannagan <kieranhannagan@Kierans-MacBook-Pro.local>
2022-03-11 21:42:42 -0600

    finished footer

--- portfolio/src/components/Nav/index.js
+++ portfolio/src/components/Nav/index.js
@@ -15,14 +15,14 @@ function Nav(props) {
   }, [currentCategory]);
 
   return (
-    <header className="flex-row px-1">
+    <header className="d-flex flex-row px-1 justify-content-between">
       <h2>
         <a data-testid="link" href="/">
           <span role="img" aria-label="camera"></span> Kieran Hannagan
         </a>
       </h2>
-      <nav>
-        <ul className="flex-row">
+      <nav className='d-flex justify-content-right p-2'>
+        <ul className="d-flex ">
           <li className={`mx-2 ${contactSelected && 'navActive'}`}>
             <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
