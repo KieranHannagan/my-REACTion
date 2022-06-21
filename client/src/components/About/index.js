b434290c374c244b1afe58707f20162a3e4560cb
Kieran Hannagan <kieranhannagan@Kierans-MacBook-Pro.local>
2022-06-21 16:37:01 -0500

    added cover photo

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <section className="my-5 container">
       <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <img src={coverImage} className="my-2" style={{ width: "25%" }} alt="cover" />
       <div className="my-2">
         <p>
          Hey there! My name is Kieran, I am an graduating senior at the University of St Thomas in St. Paul. 
